39ffaa426304e0c483b2054459615456b58b25a2
Bruno Matias Casas <leguenta@hotmail.com>
2018-09-24 16:15:16 -0300

    Database setting

--- pdfConverter/settings.py
+++ pdfConverter/settings.py
@@ -74,14 +74,15 @@ WSGI_APPLICATION = 'pdfConverter.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'HOST': 'db',
-            'PORT': 5432,
-            }
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
