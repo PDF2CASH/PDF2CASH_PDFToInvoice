3840f2e17869c07f8e89155dcbd5aedf3a8b8163
Bruno Matias Casas <leguenta@hotmail.com>
2018-09-26 14:58:03 -0300

    Rewrite invoice create for pdf converter
    Co-authored-by: carlosaragon <carloseraragon@gmail.com>
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

--- invoice/views.py
+++ invoice/views.py
@@ -16,3 +16,39 @@ class InvoiceViewSet(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
 
+    def create(self, request):
+
+        data = request.data
+
+        dict_invoice = {}
+
+        #-----------------salva-pdf---------------------#
+
+        pdf = PDF()
+
+        pdf.pdf = data.get('file')
+
+        pdf.save()
+
+        dict_invoice['pdf'] = pdf
+
+        #---------------Converter-para-texto-------------#
+
+        filename = str(pdf.pdf).split('/')[2]
+        
+        pdfFileObj = open("invoice/images/" + filename,'rb')
+       
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+       
+        num_pages = pdfReader.numPages
+        count = 0
+        text = ""
+        
+        while count < num_pages:
+            pageObj = pdfReader.getPage(count)
+            count +=1
+            text += pageObj.extractText()
+
+
+
+        
