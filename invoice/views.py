3ec8649790d15f2e1e041bb694fa1093f5ea3a42
Bruno Matias Casas <leguenta@hotmail.com>
2018-10-01 18:30:12 -0300

    Add tool to convert pdf; refs: #74,#75
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>
    Co-authored-by: carlosaragon <carloseraragon@gmail.com>
    Co-authored-by: Brian2397 <brianlui2387@gmail.com>

--- invoice/views.py
+++ invoice/views.py
@@ -9,8 +9,36 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import re
 from django.shortcuts import get_object_or_404
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+import pdftotext
 
 
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
+
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text  
 class InvoiceViewSet(viewsets.ModelViewSet):
 
     queryset = Invoice.objects.all()
