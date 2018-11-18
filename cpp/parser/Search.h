dcbd2f99dc5f08a0d933e484131e233d2efd4a63
jclitiwn <j.litwin@live.com>
2018-11-18 16:42:51 -0200

    add levenstein algorithm with trie;
    refs fga-eps-mds/2018.2-PDF2CASH#166
    
    Co-authored-by: danielmarques28 <danielmarques7@hotmail.com>

--- cpp/parser/Search.h
+++ cpp/parser/Search.h
@@ -6,6 +6,39 @@
 
 #include <QMap>
 #include <QList>
+#include <QVector>
+
+struct TrieNode
+{
+    // The set with all the letters which this node is prefix
+    QMap<QChar, TrieNode*> next;
+
+    // If word is equal to "" is because there is no word in the
+    //  dictionary which ends here.
+    QString word;
+
+    TrieNode() : next(QMap<QChar, TrieNode*>()) {}
+
+    void insert(QString w)
+    {
+        w = QString("$") + w;
+
+        int sz = w.size();
+
+        TrieNode* n = this;
+        for (int i = 0; i < sz; ++i)
+        {
+            if (n->next.find(w[i]) == n->next.end())
+            {
+                n->next[w[i]] = new TrieNode();
+            }
+
+            n = n->next[w[i]];
+        }
+
+        n->word = w;
+    }
+};
 
 class Search
 {
@@ -16,7 +49,11 @@ public:
 
     QString Convert(QString str, bool useAbbreviation = true);
 
+    // Function's related to search string's.
+    int SearchByDistance(QString word);
+
 private:
+    // Function's related to edit string's.
     bool ToLowerCase(QString* str);
     bool RemoveAccents(QString* str);
     bool RemoveSpecialCharacter(QString* str);
@@ -25,6 +62,9 @@ private:
     bool RemoveAbbreviation(QString* str);
 
     bool RemoveAbnormal(QString* str);
+
+    // Function's related to search string's.
+    void SearchImpl(TrieNode* tree, QChar ch, QVector<int> last_row, const QString& word);
 
 private:
     QMap<QString, QList<QString>> _abbreviationsMap;
