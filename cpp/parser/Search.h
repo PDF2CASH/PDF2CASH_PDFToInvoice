a9027aca31cd051d9aad42aac1d902ccde7a9984
jclitiwn <j.litwin@live.com>
2018-11-18 02:01:02 -0200

    add abbreviation function;
    refs fga-eps-mds/2018.2-PDF2CASH#110
    
    
    Co-authored-by: danielmarques28 <danielmarques7@hotmail.com>

--- cpp/parser/Search.h
+++ cpp/parser/Search.h
@@ -2,13 +2,20 @@
 #define SEARCH_H
 
 #include <QString>
+#include <QStringList>
+
+#include <QMap>
+#include <QList>
 
 class Search
 {
 public:
     Search();
 
+    void Initialization();
+
     QString Convert(QString str);
+    QStringList Split(QString str);
 
 private:
     bool ToLowerCase(QString* str);
@@ -16,6 +23,10 @@ private:
     bool RemoveSpecialCharacter(QString* str);
     bool RemoveExtraCharacter(QString* str, const QChar c);
 
+    bool RemoveAbbreviation(QString* str);
+
+private:
+    QMap<QString, QList<QString>> _abbreviationsMap;
 };
 
 #endif // SEARCH_H
