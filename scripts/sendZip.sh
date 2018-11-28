2c7c42b1decd06e09bec8b956c90a7b4d87deb60
Daniel Marques <danielmarques7@hotmail.com>
2018-11-28 03:51:03 +0000

    Add pandas in the pyinstaller build

--- scripts/sendZip.sh
+++ scripts/sendZip.sh
@@ -2,6 +2,5 @@
 
 apt get install zip
 apt get install curl
-cd ..
 zip -r  ./pdftoinvoice.zip ./build
 curl -F 'file=@./pdftoinvoice.zip' https://pdf2cash-cloudupdater.herokuapp.com/api/system/pdftoinvoice/
